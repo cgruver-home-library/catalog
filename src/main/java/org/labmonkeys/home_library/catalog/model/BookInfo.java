27ee769a45d392da282f383b137569a1dc776ea4
Charro Gruver <charro_gruver@yahoo.com>
2021-01-04 11:29:00 -0500

    Merge CLG WIP

--- src/main/java/org/labmonkeys/home_library/catalog/model/BookInfo.java
+++ src/main/java/org/labmonkeys/home_library/catalog/model/BookInfo.java
@@ -6,17 +6,11 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-
-import org.hibernate.annotations.GenericGenerator;
-
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,16 +18,12 @@ import lombok.EqualsAndHashCode;
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = false)
-@Table(name = "book_info", uniqueConstraints = {@UniqueConstraint(columnNames = {"isbn"}, name = "isbn")}, indexes = { @Index(name = "idx_isbn", columnList = "isbn"), @Index(name = "idx_title", columnList = "title")} )
+@Table(name = "book_info", indexes = {@Index(name = "idx_title", columnList = "title")} )
 public class BookInfo extends PanacheEntityBase {
 
     @Id()
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
-    private Long id;
-
-    @Column()
-    private String isbn;
+    @Column(name = "catalog_id")
+    private String catalogId;
 
     @Column()
     private String title;
@@ -54,7 +44,7 @@ public class BookInfo extends PanacheEntityBase {
     @OrderBy("name ASC")
     private List<Author> authors;
 
-    public static BookInfo getBookInfoByIsbn(String isbn){
-        return find("isbn", isbn).firstResult();
-    }
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "bookInfo", cascade = CascadeType.ALL)
+    @OrderBy("isbn ASC")
+    private List<ISBN> isbns;
 }
--- src/main/java/org/labmonkeys/home_library/catalog/service/BookCatalogService.java
+++ src/main/java/org/labmonkeys/home_library/catalog/service/BookCatalogService.java
@@ -1,8 +1,5 @@
 package org.labmonkeys.home_library.catalog.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -10,15 +7,11 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.labmonkeys.home_library.catalog.aop.Audited;
 import org.labmonkeys.home_library.catalog.mapper.BookInfoMapper;
-import org.labmonkeys.home_library.catalog.model.Author;
 import org.labmonkeys.home_library.catalog.model.BookInfo;
+import org.labmonkeys.home_library.catalog.model.ISBN;
 import org.labmonkeys.home_library.catalog.rest.BookCatalogException;
-import org.labmonkeys.home_library.catalog.rest.dto.AuthorDTO;
 import org.labmonkeys.home_library.catalog.rest.dto.BookInfoDTO;
 import org.labmonkeys.home_library.openlibrary.rest.api.OpenLibrary;
-import org.labmonkeys.home_library.openlibrary.rest.dto.AuthorOL;
-import org.labmonkeys.home_library.openlibrary.rest.dto.BookInfoDetailOL;
-import org.labmonkeys.home_library.openlibrary.rest.dto.BookInfoOL;
 
 @ApplicationScoped
 public class BookCatalogService {
@@ -37,29 +30,14 @@ public class BookCatalogService {
         LOG.info("getBookInfoByIsbn method invoked!");
         BookInfoDTO bookInfoDto = null;
         BookInfo bookInfoEntity = null;
-        bookInfoEntity = BookInfo.getBookInfoByIsbn(isbn);
+        bookInfoEntity = ISBN.getBookInfoByIsbn(isbn);
         if (bookInfoEntity == null) {
             isbn = "ISBN:" + isbn;
-            BookInfoOL bookInfoOL = openLibrary.getBookInfo(isbn, "json", "data");
-            BookInfoDetailOL bookInfoDetails = bookInfoOL.getDetails();
-            bookInfoDto = new BookInfoDTO();
-            ArrayList<AuthorDTO> authors = new ArrayList<AuthorDTO>();
-            for (AuthorOL author : bookInfoDetails.getAuthors()) {
-                AuthorDTO dto = new AuthorDTO();
-                dto.setName(author.getName());
-                dto.setOpenLibraryUrl(author.getUrl());
-                authors.add(dto);
-            }
-            bookInfoDto.setAuthors(authors);
-            bookInfoDto.setCoverImageUrl(bookInfoDetails.getCover().getSmall());
-            bookInfoDto.setIsbn(bookInfoOL.getIsbn());
-            bookInfoDto.setNumberOfPages(bookInfoDetails.getNumberOfPages());
-            bookInfoDto.setOpenLibraryUrl(bookInfoDetails.getUrl());
-            bookInfoDto.setPublishDate(bookInfoDetails.getPublishDate());
-            bookInfoDto.setTitle(bookInfoDetails.getTitle());
-            
+            bookInfoDto = bookInfoMapper.bookInfoOlToBookInfoDTO(openLibrary.getBookInfo(isbn, "json", "data"));
+            bookInfoDto.setInCatalog(false);
         } else {
             bookInfoDto = bookInfoMapper.bookInfoEntityToDto(bookInfoEntity);
+            bookInfoDto.setInCatalog(true);
         }
 
         return bookInfoDto;
@@ -68,13 +46,6 @@ public class BookCatalogService {
     @Transactional
     public void saveBookInfo(BookInfoDTO dto) throws BookCatalogException {
         BookInfo bookInfoEntity = bookInfoMapper.bookInfoDtoToEntity(dto);
-        List<Author> authorEntities = new ArrayList<Author>();
-        for (AuthorDTO authorDto : dto.getAuthors()) {
-            Author authorEntity = bookInfoMapper.authorDtoToEntity(authorDto);
-            authorEntity.setBookInfo(bookInfoEntity);
-            authorEntities.add(authorEntity);
-        }
-        bookInfoEntity.setAuthors(authorEntities);
         BookInfo.persist(bookInfoEntity);
     }
 }
